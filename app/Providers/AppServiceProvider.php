2c576e6c786e3cfa6339795eee9e3bd2db85ed89
Dmytro Hanzha <hanzhadima@gmail.com>
2024-09-23 18:40:49 +0100

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use App\Events\SubmissionSaved;
+use App\Listeners\SubmissionNotification;
+use App\Services\Submission;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Facades\Event;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
